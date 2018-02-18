a19fca32934faebf20f16f8dcd8731308ee4843c
Sofiane Gargouri <contact@sofianeg.com>
2018-02-18 23:08:36 +0100

    Trimming commands

--- src/handlers/root-handler.js
+++ src/handlers/root-handler.js
@@ -1,7 +1,7 @@
 export default class RootHandler {
   constructor(props) {
     this.message = props.message
-    this.message.splitContent = this.message.content.trim().split(' ')
+    this.message.splitContent = this.message.content.trim().replace(/  +/g, ' ').split(' ')
     this.message.authorTag = `${this.message.author.username}#${this.message.author.discriminator}`
   }
 }
