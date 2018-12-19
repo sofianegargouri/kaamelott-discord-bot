e97db400056a8511a288b2d8a4e5fc02d294d455
Sofiane Gargouri <contact@sofianeg.com>
2018-12-19 08:55:42 +0100

    Updating status each 5 seconds

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import KaamelottHandler from './handlers/kaamelott-handler'
 
 const client = new Discord.Client()
 
+let watching = true
+
 dotenv.config()
 
-client.on('ready', () =>
-  client.user.setActivity('Kaamelott', {type: 'WATCHING'}))
+client.on('ready', () => {
+  setInterval(() => {
+    if (watching) {
+      client.user.setActivity('Kaamelott', {type: 'WATCHING'})
+    } else {
+      client.user.setActivity('!kmlt help', {type: 'PLAYING'})
+    }
+  }, 5000)
+})
 
-client.on('message', message => 
+client.on('message', message =>
   (message.content.startsWith('!kaamelott')
   || message.content.startsWith('!kmlt'))
   && new KaamelottHandler({ message }))
